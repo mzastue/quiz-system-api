4431c87c5374b6de09cea7eb21e937fd17422145
mateusz.littwin <mateusz.littwin@gforces.pl>
2019-02-20 23:16:51 +0100

    Add cors and body-parser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import router from './config/router';
 import './config/dotenv';
 import './config/db';
@@ -8,7 +9,9 @@ const app = express();
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 
-app.use('/api', cors(), router(express));
+app.use(bodyParser());
+app.use(cors());
+app.use('/api', router(express));
 
 app.listen(port, () => {
   console.log(`Server listening on ${host}:${port}`);
